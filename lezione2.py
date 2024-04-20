11673eb0198510586027b3f2c601970f26c5187e
elmoriconi <ele.9.9@virgilio.it>
2024-04-20 23:36:24 +0200

    3-8 to 3-9

--- lezione2.py
+++ lezione2.py
@@ -52,3 +52,40 @@ for i in range(len(guestlist)):
     print(f"Dear {guestlist[i]}, I'm happy to confirm you are still invited to dinner next week.")
 del guestlist[0:2]
 print(guestlist)
+
+
+#3-8. Seeing the World: Think of at least five places in the world you’d like to visit.
+#• Store the locations in a list. Make sure the list is not in alphabetical order.
+#• Print your list in its original order. Don’t worry about printing the list neatly; just print it as a raw Python list.
+#• Use sorted() to print your list in alphabetical order without modifying the actual list.
+#• Show that your list is still in its original order by printing it.
+#• Use sorted() to print your list in reverse-alphabetical order without changing the order of the original list.
+#• Show that your list is still in its original order by printing it again.
+#• Use reverse()  to change the order of your list. Print the list to show that its order has changed.
+#• Use reverse() to change the order of your list again. Print the list to show it’s back to its original order.
+#• Use sort() to change your list so it’s stored in alphabetical order. Print the list to show that its order has been changed.
+#• Use sort() to change your list so it’s stored in reverse-alphabetical order.
+#Print the list to show that its order has changed.
+
+locations: list = ["Iceland", "Egypt", "Spain", "Scotland", "Japan"]
+print(locations)
+print(sorted(locations))
+print(locations)
+print(sorted(locations, reverse=True))
+print(locations)
+locations.reverse()
+print(locations)
+locations.reverse()
+print(locations)
+locations.sort()
+print(locations)
+locations.sort(reverse=True)
+print(locations)
+
+
+#3-9. Dinner Guests: Working with one of the programs from Exercises 3, use len() to print a message indicating the number of people you’re inviting to dinner.
+guestlist: list = ["William Shakespeare", "Dante Alighieri", "Hozier", "Marsha P. Johnson", "Kabi Nagata", "David Tennant"] #I created the list again otherwise its length would be 0 (we emptied it earlier)
+print(f"I am inviting {len(guestlist)} to my dinner party.")
+
+
+#3-10. Every Function: Think of things you could store in a list. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items and then uses each function introduced in this chapter at least once.
