a6ca486ece993cdbc14f34c352548e046807ad07
elmoriconi <ele.9.9@virgilio.it>
2024-04-22 10:47:07 +0200

    es. 3

--- lezione4bis.py
+++ lezione4bis.py
@@ -10,7 +10,7 @@ y: float = 5
 result: list[float] = subtract_all(x, y)
 print(result)
 
-def suBtract_all2(x: list[float], y: float) -> list[float]:
+def suBtract_all2(x: list[float], y: list[float]) -> list[float]:
     result: list[float] = []
     for i in range(len(x)):
         z: float = x[i] - y[i]
@@ -21,3 +21,21 @@ a: list[float] = [1,2,3,4,5]
 b: list[float] = [2,3,4,5,6]
 result2: list[float] = suBtract_all2(a, b)
 print(result2)
+
+def suBtract_all3(x: list[float], y: list[float]) -> list[float]:
+    result: list[float] = []
+    if len(x) >= len(y):
+        for i in range(len(y)):
+            z: float = x[i] - y[i]
+        result.append(z)
+    else:
+        for i in range(len(x)):
+            z: float = x[i] - y[i]
+            result.append(z)
+    return result
+
+c: list[float] = [1,2,3,4,5]
+d: list[float] = [2,3,4,5,6,7]
+result3: list[float] = suBtract_all3(c, d)
+print(result3)
+
