62b88ecf4678f150d1f55dfc94c0abbf89ecc700
elmoriconi <ele.9.9@virgilio.it>
2024-04-23 11:57:34 +0200

    4-7

--- lezione3/lezione3.py
+++ lezione3/lezione3.py
@@ -28,7 +28,7 @@ for i in range(1, 21):
     print(i)
 
 #4-4. One Million: Make a list of the numbers from one to one million, and then use a for loop to print the numbers. (If the output is taking too long, stop it by pressing CTRL-C or by closing the output window.)
-"""
+""" Commented to avoid running it each time and wasting time
 numbers: list[int] = []
 for i in range(1, 1000001):
     numbers.append(i)
@@ -45,14 +45,17 @@ print(sum(numbers2))
 
 #4-6. Odd Numbers: Use the third argument of the range() function to make a list of the odd numbers from 1 to 20. Use a for loop to print each number.
 
-numbers2: list[int] = []
+numbers3: list[int] = []
 for i in range(1, 20, 2):
-    numbers2.append(i)
+    numbers3.append(i)
     print(i)
 
 #4-7. Threes: Make a list of the multiples of 3, from 3 to 30. Use a for loop to print the numbers in your list.
 
-
+multiples3: list[int] = []
+for i in range(3, 31, 3):
+    multiples3.append(i)
+    print(i)
 
 #4-8. Cubes: A number raised to the third power is called a cube. For example, the cube of 2 is written as 2**3 in Python. Make a list of the first 10 cubes (that is, the cube of each integer from 1 through 10), and use a for loop to print out the value of each cube.
 
