4b323573aefb3c9af298cad7c3a2d156ebf01911
elmoriconi <ele.9.9@virgilio.it>
2024-04-28 19:35:40 +0200

    5-11

--- lezione3/lezione3.py
+++ lezione3/lezione3.py
@@ -356,3 +356,14 @@ for elem in new_users:
     #• Store the numbers 1 through 9 in a list.
     #• Loop through the list.
     #• Use an if-elif-else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on a separate line
+
+numbers_list: list[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+for elem in numbers_list:
+    if elem == 1:
+        print(f"{elem}st")
+    elif elem == 2:
+        print(f"{elem}nd")
+    elif elem == 3:
+        print(f"{elem}rd")
+    else:
+        print(f"{elem}th")
