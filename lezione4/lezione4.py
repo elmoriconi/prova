e82d2d37446430264a0b0bcf4ce73cc8c63cef6d
elmoriconi <ele.9.9@virgilio.it>
2024-04-29 17:48:27 +0200

    8-6

--- lezione4/lezione4.py
+++ lezione4/lezione4.py
@@ -48,7 +48,12 @@ describe_city(name = "Oslo")
 
 #8-6. City Names: Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this: "Santiago, Chile". Call your function with at least three city-country pairs, and print the values that are returned.
 
+def city_country(name: str, country: str) -> str:
+    print(f"{name}, {country}")
 
+city_country(name = "Madrid", country = "Spain")
+city_country(name = "Rome", country = "Italy")
+city_country(name = "Paris", country = "France")
 
 #8-7. Album: Write a function called make_album() that builds a dictionary describing a music album. The function should take in an artist name and an album title, and it should return a dictionary containing these two pieces of information. Use the function to make three dictionaries representing different albums. Print each return value to show that the  dictionaries are storing the album information correctly. Use None to add an optional parameter to make_album() that allows you to store the number of songs on an album. If the calling line includes a value for the number of songs, add that value to the album’s dictionary. Make at least one new function call that includes the number of songs on an album.
 
