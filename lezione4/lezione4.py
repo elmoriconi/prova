54cc530c166cf38e5fd246f25766a8f7e2c34f97
elmoriconi <ele.9.9@virgilio.it>
2024-04-29 17:44:31 +0200

    8-4 and 8-5

--- lezione4/lezione4.py
+++ lezione4/lezione4.py
@@ -30,18 +30,21 @@ make_tshirt2(size = "M", text = "Forza Roma")
 
 #8-4. Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love Python. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
 
-def make_tshirt3() -> str:
-    size: str = "L"
-    text2: str = "I love Python"
-    print(f"The tshirt should be a size {size} and should read '{text2}'.")
-    print(f"The tshirt should be a size 'M' and should read '{text2}'.")
-    print(f"The tshirt should be a size 'S' and should read 'I hate Python'.")
+def make_tshirt3(size: str = "L", text: str = "I love Python") -> str:
+    print(f"The tshirt should be a size {size} and should read '{text}'.")
+    print(f"The tshirt should be a size M and should read '{text}'.")
+    print(f"The tshirt should be a size S and should read 'I hate Python'.")
 
 make_tshirt3()
 
 #8-5. Cities: Write a function called describe_city() that accepts the name of a city and its country. The function should print a simple sentence, such as Reykjavik is in Iceland. Give the parameter for the country a default value. Call your function for three different cities, at least one of which is not in the default country.
 
+def describe_city(name: str, country: str = "Sweden") -> str:
+    print(f"{name} is in {country}.")
 
+describe_city(name = "Stockholm")
+describe_city(name = "Malmo")
+describe_city(name = "Oslo")
 
 #8-6. City Names: Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this: "Santiago, Chile". Call your function with at least three city-country pairs, and print the values that are returned.
 
