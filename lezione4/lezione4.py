91b3b81aeb5b3b258cb9d688ba3110d258d001db
elmoriconi <ele.9.9@virgilio.it>
2024-04-29 18:00:39 +0200

    8-7

--- lezione4/lezione4.py
+++ lezione4/lezione4.py
@@ -55,9 +55,20 @@ city_country(name = "Madrid", country = "Spain")
 city_country(name = "Rome", country = "Italy")
 city_country(name = "Paris", country = "France")
 
-#8-7. Album: Write a function called make_album() that builds a dictionary describing a music album. The function should take in an artist name and an album title, and it should return a dictionary containing these two pieces of information. Use the function to make three dictionaries representing different albums. Print each return value to show that the  dictionaries are storing the album information correctly. Use None to add an optional parameter to make_album() that allows you to store the number of songs on an album. If the calling line includes a value for the number of songs, add that value to the album’s dictionary. Make at least one new function call that includes the number of songs on an album.
+#8-7. Album: Write a function called make_album() that builds a dictionary describing a music album. The function should take in an artist name and an album title, and it should return a dictionary containing these two pieces of information. Use the function to make three dictionaries representing different albums. Print each return value to show that the dictionaries are storing the album information correctly. Use None to add an optional parameter to make_album() that allows you to store the number of songs on an album. If the calling line includes a value for the number of songs, add that value to the album’s dictionary. Make at least one new function call that includes the number of songs on an album.
 
+def make_album(artist: str, title: str, songs: int) -> dict:
+    album: dict = {
+        "artist": artist,
+        "title": title,
+        "songs": songs
+    }
+    print(album)
 
+make_album(artist = "Hozier", title = "Wasteland, Baby!", songs = None)
+make_album(artist = "Harry Styles", title = "Harry's House", songs = None)
+make_album(artist = "Conan Gray", title = "Superache", songs = None)
+make_album(artist = "Taylor Swift", title = "The Tortured Poets Department", songs = 31)
 
 #8-8. User Albums: Start with your program from Exercise 8-7. Write a while loop that allows users to enter an album’s artist and title. Once you have that information, call make_album() with the user’s input and print the dictionary that’s created. Be sure to include a quit value in the while loop.
 
