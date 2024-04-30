449967dbfb1601990b32ff7eab2bf19291eea798
elmoriconi <ele.9.9@virgilio.it>
2024-04-30 17:54:11 +0200

    8-13

--- lezione4/lezione4.py
+++ lezione4/lezione4.py
@@ -136,9 +136,17 @@ sandwiches("tomato", "mayo", "tuna")
 sandwiches("ham", "lettuce", "cheese", "bacon")
 sandwiches("chicken", "tomato")
 
-#8-13. User Profile:  Build a profile of yourself by calling build_profile(), using your first and last names and three other key-value pairs that describe you. All the values must be passed to the function as parameters. The function then must return a string such as "Eric Crow, age 45, hair brown, weight 67"
+#8-13. User Profile: Build a profile of yourself by calling build_profile(), using your first and last names and three other key-value pairs that describe you. All the values must be passed to the function as parameters. The function then must return a string such as "Eric Crow, age 45, hair brown, weight 67"
 
+def build_profile(first_name: str, last_name: str, age: int, hair_colour: str, eye_colour: str) -> str:
+    print(f"{first_name} {last_name}, age {age}, hair {hair_colour}, eyes {eye_colour}")
 
+first_name: str = "Elena"
+last_name: str = "Moriconi"
+age: int = 21
+hair_colour: str = "brown"
+eye_colour: str = "brown"
+build_profile(first_name, last_name, age, hair_colour, eye_colour)
 
 #8-14. Cars: Write a function that stores information about a car in a dictionary. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Your function should work for a call like this one: car = make_car('subaru', 'outback', color='blue', tow_package=True) Print the dictionary that’s returned to make sure all the information was stored correctly. 
 
