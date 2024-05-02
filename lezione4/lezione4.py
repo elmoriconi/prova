ca40cae7a6c64dbf4714d5b2166a3de2f6118ad1
elmoriconi <ele.9.9@virgilio.it>
2024-05-02 15:50:47 +0200

    spelling mistake

--- lezione4/lezione4.py
+++ lezione4/lezione4.py
@@ -9,7 +9,7 @@ print(display_message())
 #8-2. Favorite Book: Write a function called favorite_book() that accepts one parameter, title. The function should print a message, such as "One of my favorite books is Alice in Wonderland". Call the function, making sure to include a book title as an argument in the function call.
 
 def favourite_book(title: str) -> str:
-    print(f"One of my favpurite books is '{title}'")
+    print(f"One of my favourite books is '{title}'")
 
 title: str = "Pride and Prejudice"
 favourite_book(title)
