17b24b513611dc0ab7d76d2d061c1c59f56d7f7e
elmoriconi <ele.9.9@virgilio.it>
2024-05-03 18:14:11 +0200

    albero binario

---
+++ binary_search.py
@@ -0,0 +1,13 @@
+def binary_search(array: list[int], x: int) -> int:
+    low = 0
+    high = len(array)
+    while low < high:
+        mid = (low + high) // 2
+        if array[mid] == x:
+            return mid
+        else:
+            if x > array[mid]:
+                low = mid + 1
+            else:
+                high = mid - 1
+    return None
--- es-del-test.py
+++ es-del-test.py
@@ -27,3 +27,5 @@ def find_lhs(notes: list[int]) -> int:
     for num in num_freq:
         max_length = max(max_length, num_freq[num] + num_freq[num+1])
     return max_length
+
+
