77802a18fe4815735fa8ced2431d23efdba0c110
elmoriconi <ele.9.9@virgilio.it>
2024-05-03 19:40:00 +0200

    halfway there hopefully?

--- binary_search.py
+++ binary_search.py
@@ -49,7 +49,7 @@ visit_tree_iterative(tree, 4)
 #BFS
 
 def visit_tree_iterative(tree:  dict[int, list[int]], root: int):
-    stack: list[int] = [root] #LIFO
+    stack: list[int] = [root] #FIFO
     while stack: #len > 0
         curr_node = stack.pop(0)
         print(curr_node)
@@ -58,3 +58,25 @@ def visit_tree_iterative(tree:  dict[int, list[int]], root: int):
             stack.append(right_child)
         if left_child:
             stack.append(left_child)
+
+
+#es: visita in ampiezza calcolando la media per livelli. dizionario con chiave: livello, valore: media
+
+def visit_tree_iterative2(tree:  dict[int, list[int]], root: int) -> dict:
+    stack: list[int] = [root] 
+    dizionario: dict = {"0": root}
+    level: int = 0
+    while stack: #len > 0
+        curr_node = stack.pop(0)
+        #print(curr_node)
+        left_child, right_child = tree.get(curr_node, [None, None])
+        if right_child:
+            stack.append(right_child)
+        if left_child:
+            stack.append(left_child)
+            level += 1
+            #dizionario.update({level: sum(stack)/len(stack)})
+            print(level, stack)
+
+tree = {4: [3, 5], 3: [2, None], 2: [None, None], 5: [4.5, 6], 4.5: [None, None], 6: [None, None]}
+visit_tree_iterative2(tree, 4)
