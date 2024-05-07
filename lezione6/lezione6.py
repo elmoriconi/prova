2c3ffe22bdd64668695290d280a888650a5c7119
elmoriconi <ele.9.9@virgilio.it>
2024-05-07 18:39:31 +0200

    9-2

--- lezione6/lezione6.py
+++ lezione6/lezione6.py
@@ -19,7 +19,15 @@ restaurant: Restaurant = Restaurant("Oishi", "giapponese")
 print(restaurant.describe_restaurant())
 print(restaurant.open_restaurant())
 
-#9-2. Three Restaurants: Start with your class from Exercise 9-1. Create three different instances from the class, and call describe_restaurant() for each instance.
+#9-2. Three Restaurants: Start with your class from Exercise 9-1. Create three different instances from the class, 
+      #and call describe_restaurant() for each instance.
+
+restaurant2: Restaurant = Restaurant("Trapizzino", "romana")
+restaurant3: Restaurant = Restaurant("Gandhi", "indiana")
+restaurant4: Restaurant = Restaurant("Ami Poke", "hawaiana")
+print(restaurant2.describe_restaurant())
+print(restaurant3.describe_restaurant())
+print(restaurant4.describe_restaurant())
 
 #9-3. Users: Make a class called User. Create two attributes called first_name and last_name, and then create several other attributes that are typically stored in a user profile. Make a method called describe_user() that prints a summary of the user’s information. Make another method called greet_user() that prints a personalized greeting to the user. Create several instances representing different users, and call both methods for each user.
 
