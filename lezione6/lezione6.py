17df34033af4a7026586e41b87a479bd883584d6
elmoriconi <ele.9.9@virgilio.it>
2024-05-07 11:17:04 +0200

    str

--- lezione6/lezione6.py
+++ lezione6/lezione6.py
@@ -1,15 +1,19 @@
 class Person:
 
 
-    def __init__(self, name, age, height, weight):
+    def __init__(self, name: str, age: int, height: float, weight:float):
         self.name = name
         self.age = age
         self.height = height
         self.weight = weight
 
 
-alice = Person("Alice W.", 45, 166, 60)
-bob = Person("Bob M.", 36, 178, 76)
+    def __str__(self) -> str:
+        return(f"Nome: {self.name}, età: {self.age}, altezza: {self.height}, peso: {self.weight}")
+
+
+alice: Person = Person("Alice W.", 45, 166, 60)
+bob: Person = Person("Bob M.", 36, 178, 76)
 
 print(f"Bob's age: {bob.age}")
 
@@ -18,12 +22,13 @@ if alice.age > bob.age:
 else:
     print(bob.name)
 
-elena = Person("Elena M.", 21, 158, 49)
-giada = Person("Giada G.", 20, 160, 55)
-gabriele = Person("Gabriele C.", 19, 180, 80)
+elena: Person = Person("Elena M.", 21, 158, 49)
+giada: Person = Person("Giada G.", 20, 160, 55)
+gabriele: Person = Person("Gabriele C.", 19, 180, 80)
 
 people: list[Person] = [alice, bob, elena, giada, gabriele]
-
+#senza __str__
+"""
 min_age: int = float('inf')
 index_min_age: int = 0
 for i in range(len(people)):
@@ -32,3 +37,7 @@ for i in range(len(people)):
         index_min_age = i
 print(f"La persona più giovane è {people[index_min_age].name} che ha {people[index_min_age].age} anni. " \
       + f"Altezza {people[index_min_age].height} cm e peso {people[index_min_age].weight} kg")
+"""
+#con __str__
+print(people[0])
+
