78e9a62299fe7d045251ee21f6c921d087aff7de
elmoriconi <ele.9.9@virgilio.it>
2024-05-07 10:40:40 +0200

    aggiornato

--- lezione6/lezione6.py
+++ lezione6/lezione6.py
@@ -1,13 +1,15 @@
 class Person:
 
 
-    def __init__(self, name, age):
+    def __init__(self, name, age, height, weight):
         self.name = name
         self.age = age
+        self.height = height
+        self.weight = weight
 
 
-alice = Person("Alice W.", 45)
-bob = Person("Bob M.", 36)
+alice = Person("Alice W.", 45, 166, 60)
+bob = Person("Bob M.", 36, 178, 76)
 
 print(f"Bob's age: {bob.age}")
 
@@ -16,9 +18,9 @@ if alice.age > bob.age:
 else:
     print(bob.name)
 
-elena = Person("Elena M.", 21)
-giada = Person("Giada G.", 20)
-gabriele = Person("Gabriele C.", 19)
+elena = Person("Elena M.", 21, 158, 49)
+giada = Person("Giada G.", 20, 160, 55)
+gabriele = Person("Gabriele C.", 19, 180, 80)
 
 people: list[Person] = [alice, bob, elena, giada, gabriele]
 
@@ -28,4 +30,5 @@ for i in range(len(people)):
     if people[i].age < min_age:
         min_age = people[i].age
         index_min_age = i
-print(f"La persona più giovane è {people[index_min_age].name} che ha {people[index_min_age].age} anni")
+print(f"La persona più giovane è {people[index_min_age].name} che ha {people[index_min_age].age} anni. " \
+      + f"Altezza {people[index_min_age].height} cm e peso {people[index_min_age].weight} kg")
