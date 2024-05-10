9c0af113ef41b1d4381191b1650207f1bcc65c6f
Nio4124 <antoniozottola41204@gmail.com>
2024-05-10 12:50:59 +0200

    es 6

--- lezione7/lezione7.py
+++ lezione7/lezione7.py
@@ -80,3 +80,47 @@ def filtra_e_mappa(prodotti: dict[str:float]) -> list[str:float]:
 
 print(filtra_e_mappa({'Penna': 15.0, 'Zaino': 50.0, 'Quaderno': 22.0}))
 print(filtra_e_mappa({'Gomma': 2.0, 'Matita': 1.0})) 
+
+
+#PARTE 1
+#Scrivi una funzione chiamata create_contact() che accetta il nome e cognome, e-mail (facoltativo) e numero di telefono (facoltativo). 
+#La funzione dovrebbe restituire un dizionario con i dettagli del contatto.
+ 
+#PARTE 2
+#Scrivi una funzione chiamata update_contact() che accetta il dizionario creato, il nome e cognome del contatto da aggiornare, e il dettaglio facoltativo da aggiornare. 
+#Questa funzione dovrebbe aggiornare il dizionario del contatto.
+
+#For example:
+#Test: contact = create_contact("Mario Rossi", email="mario.rossi@gmail.com", telefono=788787)
+#print(create_contact("Mario Rossi", email="mario.rossi@gmail.com", telefono=788787))
+#print(update_contact(contact, "Mario Rossi", telefono=123456789)) 
+#Result: {'profile': 'Mario Rossi', 'email': 'mario.rossi@gmail.com', 'telefono': 788787}
+#{'profile': 'Mario Rossi', 'email': 'mario.rossi@gmail.com', 'telefono': 123456789}
+
+def create_contact(name: str, email: str=None, telefono: int=None) -> dict:
+    contact_info: dict = {}
+    contact_info.update({"profile": name})
+    if email:
+        contact_info.update({"email": email})
+    if telefono:
+        contact_info.update({"telefono": telefono})    
+    return contact_info
+
+def update_contact(dictionary: dict, name: str, email: str =None, telefono: int=None) -> dict:
+    for key, value in dictionary.items():
+        if key == "name":
+            if value != name:
+                dictionary.update({key: name})
+        if email:
+            if key == "email":
+                if value != email:
+                    dictionary.update({key: email})
+        if telefono:
+            if key == "telefono":
+                if value != telefono:
+                    dictionary.update({key: telefono})
+    return dictionary
+
+contact = create_contact("Mario Rossi", email="mario.rossi@gmail.com", telefono=788787)
+print(create_contact("Mario Rossi", email="mario.rossi@gmail.com", telefono=788787))
+print(update_contact(contact, "Mario Rossi", telefono=123456789))
