3ffaf86d7f43aacdb81b89b9333e76c52f46ee51
Nio4124 <antoniozottola41204@gmail.com>
2024-05-15 16:45:47 +0200

    improved bubble sort

--- lezione9/lezione9.py
+++ lezione9/lezione9.py
@@ -1,8 +1,22 @@
-#bubble sort
+#bubble sort => 64 iterazioni = n**2
 
 def bubble_sort(x: list[int]):
-    for i in range(len(x)):
+    for _ in range(len(x)):      # _ si usa quando una variabile non è utilizzata
         for j in range(len(x) - 1):
+            if x[j] > x[j+1]:
+                temp: int = x[j]
+                x[j] = x[j+1]
+                x[j+1] = temp
+
+l: list[int] = [235, 53, 5, 43, 567]
+bubble_sort(l)
+print(l)
+
+#improved bubble sort => 36 iterazioni 
+
+def bubble_sort2(x: list[int]):
+    for i in range(len(x)):
+        for j in range(len(x) - i - 1):
             if x[j] > x[j+1]:
                 temp: int = x[j]
                 x[j] = x[j+1]
