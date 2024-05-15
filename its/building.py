8f46006fe9e07e0ac4ec463b81f86dcfc7c4ab8b
Nio4124 <antoniozottola41204@gmail.com>
2024-05-15 17:52:05 +0200

    its

--- its/building.py
+++ its/building.py
@@ -1,49 +1,41 @@
-
 from room import Room
 
 class Building:
-
-    def __init__(self, name: str, address: str, floors: int):
+    
+    def __init__(self, name: str, address: str, floors: tuple[int, int]):
         self.name: str = name
         self.address: str = address
-        self.floors: int = floors
+        self.floors: tuple[int, int] = floors
         self.rooms: list[Room] = []
-
-    def add_rooms(self, room: Room):
+        
+    def add_room(self, room: Room) -> bool:
         lower, upper = self.get_floors()
-        if room not in self.get_rooms() and lower <= self.get_floors() <= upper:
+        if room not in self.get_rooms() and lower <= room.floor <= upper:
             self.rooms.append(room)
             return True
         return False
-
-    def remove_room(self, room: Room):
+    
+    def remove_room(self, room: Room) -> bool:
         if room in self.get_rooms():
             self.rooms.remove(room)
-        #manca qualche cosa
-
-    def set_name(self, name: str):
-        self.name = name
-
-    def set_floors(self, floors: int):
-        self.floor = floors
-
-    def set_address(self, address: str):
-        self.address = address
-
+            return True
+        return False
+        
     def get_name(self) -> str:
         return self.name
-
-    def get_floors(self) -> int:
-        return self.floors
     
-    def get_address(self) -> int:
+    def get_address(self) -> str:
         return self.address
+    
+    def get_floors(self) -> tuple[int, int]:
+        return self.floors
     
     def get_rooms(self) -> list[Room]:
         return self.rooms
-
+    
     def __str__(self) -> str:
-        s: str = f"Edificio: {self.get_name()}, indirizzo: {self.get_address()}, piani: {self.get_floors()}"
-        s += ""
+        s: str = f'Building(name={self.get_name()}, address={self.get_address()}, floors={self.get_floors()})'
+        s += "\nWith Rooms:\n"
         for room in self.get_rooms():
-            s += ""
+            s += room.__str__() + "\n"
+        return s[:-1]
--- its/room.py
+++ its/room.py
@@ -1,22 +1,22 @@
 class Room:
-
+    
     def __init__(self, name: str, floor: int, num_seats: int):
-        self.name: str = name
-        self.floor: int = floor
-        self.num_seats: int = num_seats
-
+        self.set_name(name)
+        self.set_floor(floor)
+        self.set_num_seats(num_seats)
+        
     def set_name(self, name: str):
         self.name = name
-
+    
     def set_floor(self, floor: int):
         self.floor = floor
-
-    def set_num_seat(self, num: int):
-        self.num_seats = num
-
+        
+    def set_num_seats(self, num_seats: int):
+        self.num_seats = num_seats
+        
     def get_name(self) -> str:
         return self.name
-
+    
     def get_floor(self) -> int:
         return self.floor
     
@@ -24,4 +24,4 @@ class Room:
         return self.num_seats
     
     def __str__(self) -> str:
-        return f"Stanza: {self.name}, piano: {self.floor}, posti: {self.num_seats}"
+        return f'Room(name={self.get_name()}, floor={self.get_floor()}, num_seats={self.get_num_seats()})'
