0f54898b76cca0010b371c23f85d37b211fb2756
elmoriconi <ele.9.9@virgilio.it>
2024-05-29 17:02:38 +0200

    finito

--- lepre_tartaruga.py
+++ lepre_tartaruga.py
@@ -1,9 +1,19 @@
 import random
 
-def visualizza_posizione(lepre: int, tartaruga: int):
+def visualizza_posizione(lepre: int, tartaruga: int, bonus: dict[int, int], malus: dict[int, int]):
     quadrati: list[None|str] = []
     for _ in range(1, 71):
         quadrati.append("_")
+    for key, value in bonus.items():
+        if key == lepre:
+            lepre += value
+        elif key == tartaruga:
+            tartaruga += value
+    for key, value in malus.items():
+        if key == lepre:
+            lepre -= value
+        elif key == tartaruga:
+            tartaruga -= value
     if lepre != tartaruga:
         quadrati[lepre] = "H"
         quadrati[tartaruga] = "T"
@@ -68,6 +78,14 @@ pioggia: bool = False
 secondi: int = 0
 stamina_lepre: int = 100
 stamina_tartaruga: int = 100
+malus: dict[int, int] = {15: 3,
+                        30: 5,
+                        45: 7,
+                        }
+bonus: dict[int, int] = {10: 5,
+                         25: 3,
+                         50: 10,
+                        }
 while lepre < 70 or tartaruga < 70:
     secondi += 1
     if secondi % 10 == 0:
@@ -83,4 +101,5 @@ while lepre < 70 or tartaruga < 70:
     elif lepre >= 70 and tartaruga >= 70:
         print("IT'S A TIE.")
     else: 
-        print(visualizza_posizione(lepre, tartaruga))
+        print(visualizza_posizione(lepre, tartaruga, bonus, malus))
+        print("\n")
