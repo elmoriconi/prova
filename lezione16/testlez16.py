2b5445d37fbab6828a340d63b0aa3f9ee38c5ca0
elmoriconi <ele.9.9@virgilio.it>
2024-06-11 18:54:40 +0200

    non ci sto capendo niente ma perché non stampa

--- lezione16/testlez16.py
+++ lezione16/testlez16.py
@@ -1,4 +1,4 @@
-class Contatore:
+"""class Contatore:
     
     def __init__(self):
         self.conteggio: int = 0
@@ -23,4 +23,83 @@ class Contatore:
 
 c = Contatore()  
 c.add1()
-c.mostra()
+c.mostra()"""
+
+class RecipeManager:
+    
+    def __init__(self):
+        self.ricette: dict[str, list[str]] = {}
+        
+    def create_recipe(self, name: str, ingredients: list[str]):
+        for key, value in self.ricette.items():
+            if name not in self.ricette.items():
+                self.ricette.key = name
+                self.ricette.value = ingredients
+            else:
+                print("Ricetta già esistente")
+        return self.ricette
+    
+    def add_ingredient(self, recipe_name: str, ingredient: str):
+        for key, value in self.ricette.items():
+            if recipe_name in self.ricette.items():
+                if ingredient not in self.ricette[recipe_name]:
+                    lista_ingredienti = self.ricette[recipe_name]
+                    self.ricette[recipe_name].append(ingredient)
+                    self.ricette.update({"recipe_name: lista_ingredienti"})
+                    return self.ricette[recipe_name]
+                else:
+                    print("Ingrediente già presente")
+            else:
+                print("Ricetta non esistente")
+    
+    def remove_ingredient(self, recipe_name: str, ingredient: str):
+        for key, value in self.ricette.items():
+            if recipe_name in self.ricette.items():
+                if ingredient in self.ricette[recipe_name]:
+                    lista_ingredienti = self.ricette[recipe_name]
+                    self.ricette[recipe_name].remove(ingredient)
+                    self.ricette.update({"recipe_name: lista_ingredienti"})
+                    return self.ricette[recipe_name]
+                else:
+                    print("Ingrediente non presente")
+            else:
+                print("Ricetta non esistente")
+            
+    def update_ingredient(self, recipe_name: str, old_ingredient: str, new_ingredient: str):
+        for key, value in self.ricette.items():
+            if recipe_name in self.ricette.items():
+                if old_ingredient in self.ricette[recipe_name]:
+                    lista_ingredienti = self.ricette[recipe_name]
+                    #.replace(old_ingredient, new_ingredient)
+                    self.ricette.update({"recipe_name: lista_ingredienti"})
+                    return self.ricette[recipe_name]
+                else:
+                    print("Ingrediente non presente")
+            else:
+                print("Ricetta non esistente")
+                
+    def list_recipes(self):
+        lista_chiavi: list[str] = []
+        for key, value in self.ricette.items():
+            lista_chiavi.append(key)
+        return lista_chiavi
+        
+    def list_ingredients(self, recipe_name: str):
+        for key, value in self.ricette.items():
+            if recipe_name in self.ricette.items():
+                lista_ingredienti = self.ricette[recipe_name]
+                return self.ricette[recipe_name]
+            else:
+                print("Ricetta non esistente")
+    
+    def search_recipe_by_ingredient(self, ingredient: str):
+        for key, value in self.ricette.items():
+            if ingredient in self.ricette[key]:
+                return self.ricette[key]
+
+manager = RecipeManager()
+print(manager.create_recipe("Pizza Margherita", ["Farina", "Acqua", "Lievito", "Pomodoro", "Mozzarella"]))
+#print(manager.add_ingredient("Pizza Margherita", "Basilico"))
+#print(manager.update_ingredient("Pizza Margherita", "Mozzarella", "Mozzarella di Bufala"))
+#print(manager.remove_ingredient("Pizza Margherita", "Acqua"))
+#print(manager.list_ingredients("Pizza Margherita"))
