951c799b5b3c6f71f74b356591b9cf07de05d71a
elmoriconi <ele.9.9@virgilio.it>
2024-06-21 13:02:17 +0200

    rendering grafico

--- lezione20/lezione20.py
+++ lezione20/lezione20.py
@@ -123,3 +123,68 @@ print(contanti1.dettagliPagamento())
 print(contanti2.dettagliPagamento())
 print(carta1.dettagliPagamento())
 print(carta2.dettagliPagamento())
+
+
+
+from abc import ABC
+
+class Forma(ABC):
+    def getArea(self):
+        pass
+    
+    def render(self):
+        pass
+
+class Quadrato(Forma):
+    def __init__(self, lato: int) -> None:
+        self.lato: int = lato
+        
+    def getArea(self):
+        return self.lato * self.lato
+    
+    def render(self):
+        render: str = "*" * self.lato + "\n"
+        for _ in range(self.lato - 2):
+            render += "*" + " " * (self.lato - 2) + "*\n"
+        render += "*" * self.lato
+        return render
+
+class Rettangolo(Forma):
+    def __init__(self, base: int, altezza: int) -> None:
+        self.base: int = base
+        self.altezza: int = altezza
+    
+    def getArea(self):
+        return self.base * self.altezza
+    
+    def render(self):
+        render: str = "*" * self.base + "\n"
+        for _ in range(self.altezza - 2):
+            render += "*" + " " * (self.base - 2) + "*\n"
+        render += "*" * self.base
+        return render
+    
+class Triangolo(Forma):
+    def __init__(self, base: int, altezza: int) -> None:
+        self.base: int = base
+        self.altezza: int = altezza
+        
+    def getArea(self):
+        return self.base * self.altezza
+    
+    def render(self):
+        render: str = "*\n"
+        temp: int
+        for i in range(self.altezza -2):
+            render += "*" +  " " * i + "*\n"
+        render += "*" * self.base
+        return render
+            
+        
+        
+quadrato: Quadrato = Quadrato(4)
+print(quadrato.render())
+rettangolo: Rettangolo = Rettangolo(base = 8, altezza = 4)
+print(rettangolo.render())
+triangolo: Triangolo = Triangolo(base=4, altezza=4)
+print(triangolo.render())
