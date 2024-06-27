005913a75832d728f76585638dc6714aa0c55d40
elmoriconi <ele.9.9@virgilio.it>
2024-06-27 18:33:43 +0200

    thread

--- lezione21/decorators.py
+++ lezione21/decorators.py
@@ -30,36 +30,68 @@ print(out_function)"""
 
 #Decorators
 
-def get_time(func):
+# def get_time(func):
 
-    def wrapper(*args):
-        import time
-        start = time.time()
-        func(*args)
-        end = time.time()
-        elapsed_time = end - start
-        print(f"{elapsed_time=}")
+#     def wrapper(*args):
+#         import time
+#         start = time.time()
+#         func(*args)
+#         end = time.time()
+#         elapsed_time = end - start
+#         print(f"{elapsed_time=}")
 
-    return wrapper
+#     return wrapper
 
-def say_hello(name:str)->None:
-    print(f"Hello, {name}")
+# def say_hello(name:str)->None:
+#     print(f"Hello, {name}")
 
-@get_time
-def say_ciao()->None:
-    print(f"Ciao, Flavio")
+# @get_time
+# def say_ciao()->None:
+#     print(f"Ciao, Flavio")
 
-@get_time
-def random_list(upper_bound: int):
+# @get_time
+# def random_list(upper_bound: int):
 
-    import random
-    import time
+#     import random
+#     import time
 
-    sleep_time = random.randint(0, upper_bound)
-    time.sleep(sleep_time)
+#     sleep_time = random.randint(0, upper_bound)
+#     time.sleep(sleep_time)
 
-say_hello = get_time(say_hello)
-say_hello("Flavio")
-say_ciao = (say_ciao)
-say_ciao()
-random_list()
+# say_hello = get_time(say_hello)
+# say_hello("Flavio")
+# say_ciao = (say_ciao)
+# say_ciao()
+# random_list()
+
+
+def generatore():
+    
+    yield "A"
+    yield "B"
+    yield "C"
+
+prova_generatore = generatore()
+
+print(next(prova_generatore))
+print(next(prova_generatore))
+print(next(prova_generatore))
+
+import time
+
+def funzione(id: int):
+
+    print(f"{id} time {time.time()}")
+    time.sleep(1.5)
+    print(f"{id} time {time.time()}")
+
+
+if __name__ == "__main__":
+    import threading
+
+    x: threading.thread = threading.Thread(target=funzione, args=(1,))
+    print(f"Prima di runnare il thread {time.time()}")
+    x.start()
+    print(f"Ho runnato il thread {time.time()}")
+    x.join()
+    print(f"Ho finito di runnare il thread??? {time.time()}")
